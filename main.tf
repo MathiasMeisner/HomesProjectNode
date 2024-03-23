420bbb0de5960b7510be9b491aef8523e4b43e6f
Mathias <mathias.meisner@gmail.com>
2024-03-24 00:24:41 +0100

    Terraform update

--- main.tf
+++ main.tf
@@ -6,11 +6,18 @@ resource "aws_instance" "example" {
   ami           = "ami-0d74f1e79c38f2933"  # Replace with the correct AMI ID for Amazon Linux 2023
   instance_type = "t3.micro"
   key_name      = "HomesNodeProject"  # Replace with your EC2 key pair name
-
-  # Reference the existing security group "launch-wizard-9"
-  security_groups = ["launch-wizard-9"]
+  subnet_id     = "subnet-0ba038746f2afd90c"
 
   tags = {
     Name = "HomesProjectNode"
+  }
+}
+
+resource "null_resource" "mongodb_setup" {
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/NewCode/HomesProjectNodeKey.pem")
+    host        = "13.50.13.35"
   }
 }
